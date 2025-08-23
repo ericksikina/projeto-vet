87b58d4770f0e1ce1994e9e465e384df97c2aed5
ericksikina <erickduartesikina@gmail.com>
2025-08-23 11:07:51 -0300

    feat: atualizar endereco buscando pelo token

--- src/main/java/com/bellapet/endereco/http/controller/EnderecoController.java
+++ src/main/java/com/bellapet/endereco/http/controller/EnderecoController.java
@@ -3,6 +3,7 @@ package com.bellapet.endereco.http.controller;
 import com.bellapet.endereco.http.request.EnderecoRequest;
 import com.bellapet.endereco.service.EnderecoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,9 @@ public class EnderecoController {
     private final EnderecoService enderecoService;
 
     @PutMapping(path = "/atulizar/{id}")
-    public ResponseEntity<Void>  atualizarEndereco(@PathVariable Long id, @RequestBody EnderecoRequest enderecoRequest) {
-        this.enderecoService.atualizarEndereco(id, enderecoRequest);
+    public ResponseEntity<Void>  atualizarEndereco(HttpServletRequest httpServletRequest
+            , @RequestBody EnderecoRequest enderecoRequest) {
+        this.enderecoService.atualizarEndereco(httpServletRequest, enderecoRequest);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/bellapet/endereco/service/EnderecoService.java
+++ src/main/java/com/bellapet/endereco/service/EnderecoService.java
@@ -1,10 +1,13 @@
 package com.bellapet.endereco.service;
 
+import com.bellapet.cliente.persistence.entity.Cliente;
+import com.bellapet.cliente.service.ClienteService;
 import com.bellapet.endereco.http.adapter.EnderecoAdapter;
 import com.bellapet.endereco.http.request.EnderecoRequest;
 import com.bellapet.endereco.persistence.entity.Endereco;
 import com.bellapet.endereco.persistence.repository.EnderecoRespository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,19 +16,15 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class EnderecoService {
     private final EnderecoRespository enderecoRespository;
+    private final ClienteService clienteService;
 
     public Endereco cadastrarEndereco(EnderecoRequest enderecoRequest) {
         return this.enderecoRespository.save(EnderecoAdapter.toEndereco(new Endereco(), enderecoRequest));
     }
 
     @Transactional
-    public void atualizarEndereco(Long idEndereco, EnderecoRequest enderecoRequest) {
-        Endereco endereco = this.buscarEnderecoPorId(idEndereco);
-        this.enderecoRespository.save(EnderecoAdapter.toEndereco(endereco, enderecoRequest));
-    }
-
-    private Endereco buscarEnderecoPorId(Long id) {
-        return this.enderecoRespository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Endereço não encontrado!"));
+    public void atualizarEndereco(HttpServletRequest httpServletRequest, EnderecoRequest enderecoRequest) {
+        Cliente cLiente = this.clienteService.buscarPorAuth(httpServletRequest);
+        this.enderecoRespository.save(EnderecoAdapter.toEndereco(cLiente.getEndereco(), enderecoRequest));
     }
 }
