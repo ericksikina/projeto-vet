181893881bdfc32bc5ed014aeb03cbc850d2646e
ericksikina <erickduartesikina@gmail.com>
2025-08-27 13:23:49 -0300

    feat: adicionar chamada de metodo ao controller de agendamento

--- src/main/java/com/bellapet/agendamento/http/controller/AgendamentoController.java
+++ src/main/java/com/bellapet/agendamento/http/controller/AgendamentoController.java
@@ -1,5 +1,6 @@
 package com.bellapet.agendamento.http.controller;
 
+import com.bellapet.agendamento.http.request.AgendamentoRequest;
 import com.bellapet.agendamento.http.response.AgendamentoResponse;
 import com.bellapet.agendamento.service.AgendamentoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -7,9 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,33 +19,46 @@ import java.util.List;
 public class AgendamentoController {
     private final AgendamentoService agendamentoService;
 
+    @GetMapping(path = "/listar-e-commerce")
     public ResponseEntity<List<AgendamentoResponse>> listarAgendamento(HttpServletRequest httpServletRequest) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(this.agendamentoService.listarAgendamento(httpServletRequest));
     }
 
+    @GetMapping(path = "/listar-dashboard")
     public ResponseEntity<List<AgendamentoResponse>> listarAgendamento() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(this.agendamentoService.listarAgendamento());
     }
 
+    @GetMapping(path = "/buscar-e-commerce/{id}")
     public ResponseEntity<AgendamentoResponse> buscarAgendamento(HttpServletRequest httpServletRequest,
                                                                  @PathVariable Long id) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(agendamentoService.buscarAgendamento(httpServletRequest, id));
     }
 
+    @GetMapping(path = "/buscar-dashboard/{id}")
     public ResponseEntity<AgendamentoResponse> buscarAgendamento(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(this.agendamentoService.buscarAgendamento(id));
     }
 
-    public ResponseEntity<Void> efetuarAgendamento(HttpServletRequest httpServletRequest) {
+    @PostMapping(path = "/efetuar")
+    public ResponseEntity<Void> efetuarAgendamento(HttpServletRequest httpServletRequest,
+                                                   @RequestBody AgendamentoRequest agendamentoRequest) {
+        this.agendamentoService.efetuarAgendamento(httpServletRequest, agendamentoRequest);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @PutMapping(path = "/remarcar")
     public ResponseEntity<Void> remarcarAgendamento(HttpServletRequest httpServletRequest,
+                                                    @RequestBody AgendamentoRequest agendamentoRequest,
                                                     @PathVariable Long id) {
+        this.agendamentoService.remarcarAgendamento(httpServletRequest, agendamentoRequest, id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    public ResponseEntity<Void> cancelarAgendamento(HttpServletRequest httpServletRequest) {
+    @PutMapping(path = "/cancelar")
+    public ResponseEntity<Void> cancelarAgendamento(HttpServletRequest httpServletRequest,
+                                                    @PathVariable Long id) {
+        this.agendamentoService.cancelarAgendamento(httpServletRequest, id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
